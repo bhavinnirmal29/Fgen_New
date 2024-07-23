862775bf9dc39a0c52d81cae32e1a89e0dbd93a8
DevDenLab <tjoshi@student.concordia.ab.ca>
2024-07-22 22:59:35 -0600

    Adjust buildpacks on Heroku

--- Fgen_New/settings.py
+++ Fgen_New/settings.py
@@ -140,9 +140,9 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
