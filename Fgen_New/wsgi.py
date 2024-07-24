a119a3ebccace8c81bb447b9950c21e08ee80856
DevDenLab <tjoshi@student.concordia.ab.ca>
2024-07-23 19:29:42 -0600

    Adjust buildpacks on Heroku

--- Fgen_New/wsgi.py
+++ Fgen_New/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xxxx.settings")
 from django.core.wsgi import get_wsgi_application
 from dynoscale.hooks.gunicorn import pre_request
 from whitenoise.django import DjangoWhiteNoise
