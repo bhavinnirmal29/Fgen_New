537c68feba26897d0c1e7bd2e84934dc47a85b1a
DevDenLab <tjoshi@student.concordia.ab.ca>
2024-07-23 19:42:26 -0600

    adding staticfiles

--- Fgen_New/wsgi.py
+++ Fgen_New/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from dynoscale.hooks.gunicorn import pre_request
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Fgen_New.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
