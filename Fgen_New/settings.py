d9587b44276ee22aa3f86227a382bb8cc469411f
DevDenLab <tjoshi@student.concordia.ab.ca>
2024-07-28 09:46:01 -0600

    modified settings for payment redirect

--- Fgen_New/settings.py
+++ Fgen_New/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-okes%*4du!r7dwy5_5tq$vpgjhlm9gs*ptdo84d_gw^l-lfph$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["fgen-71037e60626c.herokuapp.com","127.0.0.1","www.fgen.ca","fgen.ca","*"]#remove the * from here.
+ALLOWED_HOSTS = ["fgen-71037e60626c.herokuapp.com","127.0.0.1","www.fgen.ca","fgen.ca"]#remove the * from here.
 
 # Application definition
 
@@ -172,6 +172,6 @@ STRIPE_SECRET_KEY_TEST ="sk_test_51NNQNSFQ7fQ9eiOGNU27BidquzSvmBAC4FztWt8jroHqHQ
 STRIPE_WEBHOOK_SECRET_TEST ="whsec_c3f5766c53f559522d1498f3e5ee3e7d3745179fad4d38e2cec694685faca7ff" #os.getenv('STRIPE_WEBHOOK_SECRET_TEST')
 PRODUCT_PRICE = "price_1PhLSdFQ7fQ9eiOG4z1Rngtx"#os.getenv('PRODUCT_PRICE')
 PRODUCT_ID='prod_QYSNWnTHKR94q7'
-REDIRECT_DOMAIN = 'http://127.0.0.1:8000'
+REDIRECT_DOMAIN = 'https://fgen.ca'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
