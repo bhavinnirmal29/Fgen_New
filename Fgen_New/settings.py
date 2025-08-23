213a191f7788cd15f071b8b64ba5b8e05e5eba0e
DevDenLab <tjoshi@student.concordia.ab.ca>
2025-08-22 20:07:10 -0600

    Fix database configuration to use PostgreSQL on Heroku

--- Fgen_New/settings.py
+++ Fgen_New/settings.py
@@ -83,12 +83,25 @@ WSGI_APPLICATION = "Fgen_New.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+import dj_database_url
+
+if DEBUG:
+    # Use SQLite for local development
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    # Use PostgreSQL on Heroku
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
